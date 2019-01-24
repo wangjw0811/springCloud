b5cc4514190e4ebfec1e143250aeb3970b370343
wangjiewang <123456>
2019-01-24 11:54:57 +0800

    feign调用

--- feign_client/src/main/java/com/example/IFeignClient.java
+++ feign_client/src/main/java/com/example/IFeignClient.java
@@ -4,12 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Component
 @FeignClient("ticket-provider")
 public interface IFeignClient {
     @RequestMapping(value = "sayhello",method =RequestMethod.GET)
-    @ResponseBody
+//    @ResponseBody
     String sayHello();
 }
